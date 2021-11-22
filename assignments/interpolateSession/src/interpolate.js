85024daab2f5ee43e2090cf369215c04b1476a27
Taner Çelik <taner.celik@softtech.com.tr>
2021-11-23 02:59:31 +0300

    all test passed
    one loop, one regex

--- assignments/interpolateSession/src/interpolate.js
+++ assignments/interpolateSession/src/interpolate.js
@@ -1,5 +1,32 @@
 const interpolate = (value, session = {}, options = {}) => {
-    //TODO
+
+    if (!value) {
+        console.log("No value param provided!")
+        return "";
+    }
+
+    if (Object.keys(options).length === 0){
+        console.log("No options param provided!!")
+        return value
+    }
+
+    if (Object.keys(session).length === 0){
+        // /\w {\w+}/gm
+        let reg_ex = new RegExp('\\w?'+options.leftDelimiter+'\\w+' + options.rightDelimiter,'gm')
+        value = value.replace(reg_ex,'')
+        return value
+    }
+
+    Object.keys(session).forEach((item => {
+        let tempItem = "";
+        tempItem = options.leftDelimiter + item + options.rightDelimiter
+        value = value.split(tempItem).join(session[item])
+    }))
+
+    return value
+
 };
+
+interpolate('Hi {firstname}, how are you today?', { }, { leftDelimiter: '{', rightDelimiter: '}' });
 
 module.exports = { interpolate }
